c37fc85cde9e7b856ab69f07970ee3d042b694cb
Andy <chaochaoqi@gmail.com>
2020-10-11 02:11:59 -0700

    WIP: minArrows solution

--- 2020/minArrows/solution.js
+++ 2020/minArrows/solution.js
@@ -2,24 +2,23 @@
  * @param {number[][]} points
  * @return {number}
  */
-
-//  INCOMPLETE.
 var findMinArrowShots = function(points) {
     if(points.length == 0) {
         return 0;
     };
     
+    points = points.sort((balloon1, balloon2) => {
+        return balloon1[1] < balloon2[1] ? -1 : 1;
+    });
+    
   	resArrays = [points[0]];
 	for(let i = 0; i < points.length; i++) {
-
 		let balloon = points[i];
         let updated = false;
 		for(let j = 0; j < resArrays.length; j++) {
-
 			let range = resArrays[j];
 			let overlap = findOverlap(balloon, range);
-            console.log(`Balloon is ${balloon}. Overlap is ${overlap}. resArray is ${resArrays}`);
-			if((overlap[0] <= overlap[1]) && ((range[0] == overlap[0]) || (range[1] == overlap[1]))) {
+ 			if((overlap[0] <= overlap[1]) && ((range[0] == overlap[0]) || (range[1] == overlap[1]))) {
 				resArrays[j] = overlap;
                 updated = true;
 			};
